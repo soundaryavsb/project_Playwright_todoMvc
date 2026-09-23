4a11368fa3de9fecd23611e1f821b6c5fa34d1e9
soundaryavsb <dsoundarya1998@gmail.com>
2026-09-23 21:03:31 +0530

    updated code

--- tests/todoMvc.spec.js
+++ tests/todoMvc.spec.js
@@ -1,10 +1,28 @@
 import {test} from "@playwright/test"
 
-test("todo mvc",async ({page}) => {
+test.only("todo mvc",async ({page}) => {
     await page.goto("https://todomvc.com/examples/react/dist/");
-    await page.waitForTimeout(3000);
-    let add=page.getByPlaceholder("What needs to be done?");
+    let add=page.getByTestId('text-input');
     await add.fill("Buy groceries");
-    
-
+    await add.press('Enter');
+    await add.fill('walk');
+    await add.press('Enter');
+    await add.fill('eat');
+    await add.press('Enter');
+    await add.fill('study');
+    await add.press('Enter');
+    await page.waitForTimeout(3000);
 })
+test('test', async ({ page }) => {
+  await page.goto('https://todomvc.com/examples/react/dist/');
+  
+  
+  await page.getByRole('listitem').filter({ hasText: 'buy grocery' }).getByTestId('todo-item-toggle').check();
+  await page.getByRole('listitem').filter({ hasText: 'eat' }).getByTestId('todo-item-toggle').check();
+  await page.getByRole('link', { name: 'All' }).click();
+  await page.getByRole('link', { name: 'Active' }).click();
+  await page.getByRole('link', { name: 'Completed' }).click();
+  await page.getByRole('button', { name: 'Clear completed' }).click();
+  await page.getByRole('link', { name: 'Active' }).click();
+  await page.getByRole('link', { name: 'All' }).click();
+});
